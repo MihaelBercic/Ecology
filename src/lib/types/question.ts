7f380c5038a593691bcaa92fa9bc0d1f611dbb81
Mihael Berčič <mihael@regnum.si>
2023-06-03 22:46:14 +0200

    Modified certifications, their rankings as well as added a new answer group of 'Plastic waste'.

--- src/lib/types/question.ts
+++ src/lib/types/question.ts
@@ -11,12 +11,29 @@ export type AnswerGroup = {
 
 export type Answer = {
     text: string,
-    points: [Category, number][],
+    points: [Certificate, number][],
     following_questions: string[]
 }
 
-export enum Category {
-    EcoLabel = "EcoLabel",
-    EPD_C2B = "EPD_C2B",
-    EPD_B2B = "EPD_B2B"
+export enum Certificate {
+    IR_EPD = "IR - EPD",
+    IR_FSC = "IR - FSC",
+    IR_PEFC = "IR - PEFC",
+    IR_CE = "IR - CE",
+    IR_HEN = "IR - HEN",
+    IR_ETA = "IR - ETA",
+    IR_CradleToCradle = "IR - Cradle to Cradle",
+    UF_FSC = "UF - FSC",
+    UF_PEFC = "UF - PEFC",
+    NordicSwan = "Nordic Swan",
+    RainforestAllianceCertified = "Rainforest Alliance Certified",
+    EMAS = "EMAS",
+    CETEM_EU_EcoLabel = "CETEM - EU Ecolabel",
+    CETEM_BLUE_Angle = "CETEM - BLUE angle",
+    CETEM_OEKOTEX = "CETEM - OEKOTEX",
+    ISO_14001 = "ISO14001",
+    Fairtrade = "Fairtrade",
+    ChinaEnvironmentalLabelling = "China Environmental Labelling",
+    NaturePLUS = "natureplus",
+    AustrianThingy = "Österreichische Umweltzeichen (UZ 07 und UZ 28)"
 }
